1e9d43dc895f30513add1ba75c4b4b501fadbc98
Thomas <alexandros.thomas@accenture.com>
2018-11-08 11:05:41 +0200

    Started editing the look of the details page

--- app/components/CourseDetails.jsx
+++ app/components/CourseDetails.jsx
@@ -1,5 +1,22 @@
 import React from 'react'
-import { Image, Grid, Row, Col, Glyphicon, ButtonToolbar, Button } from 'react-bootstrap'
+import { Image, Grid, Row, Col, Glyphicon, ButtonToolbar, Button, Thumbnail, Panel } from 'react-bootstrap'
+import { S_IFMT } from 'constants';
+
+const onBackStyle = {
+    zIndex: -1
+};
+
+const onTopStyle = {
+    zIndex: 1
+}
+
+const panelStyle= {
+    height: '70%',
+    width: '100%',
+    overflow: 'hidden'
+};
+
+
 
 const CourseDetails = ({id, title,imagePath,price,open,duration,dates, description}) => (
     <Grid>
@@ -9,37 +26,27 @@ const CourseDetails = ({id, title,imagePath,price,open,duration,dates, descripti
             </Col>
         </Row>
         <Row>
-            <Col md={12}>
-                <Image src={imagePath} mode='fit'/>
-            </Col>
-        </Row>
-        <Row>
-            <Col md={6}>
-                <p><strong>Price:</strong>&nbsp;{price.normal}&nbsp; <Glyphicon glyph='glyphicon glyphicon-euro' /></p>
-            </Col>
-            <Col md={6}>
-                <p><strong>Duration:</strong>&nbsp;{duration}</p>
-            </Col>
-        </Row>
-        <Row>
-            <Col md={6}>
-                <p><strong>Bookable:</strong>&nbsp;{open ? <Glyphicon glyph='glyphicon glyphicon-ok' /> : '' }</p>
-            </Col>
-            <Col md={6}>
-                <p><strong>Dates:</strong>&nbsp;{dates.start_date}&minus;{dates.end_date}</p>
-            </Col>
-        </Row>
-        <Row>
-            <Col md={12} dangerouslySetInnerHTML={{__html: description}}>
-            </Col>
-        </Row>
-        <Row>
-            <Col>
-                <ButtonToolbar>
-                    <Button bsStyle='primary'>Edit</Button>
-                    <Button bsStyle='danger'>Delete</Button>
-                </ButtonToolbar>
-            </Col>
+        <Col xs={8} md={12}>
+                <Panel className='carousel'>
+                    <Image src={imagePath} alt="242x200" responsive style={panelStyle}/>
+                    <p className='col-md-6'>
+                        <strong>Price:</strong>&nbsp;{price.normal}&nbsp;<Glyphicon glyph='glyphicon glyphicon-euro' /><br/>
+                        <strong>Duration:</strong>&nbsp;{duration}
+                    </p>
+                    <p className='col-md-6'>
+                        <strong>Bookable:</strong>&nbsp;{open ? <Glyphicon glyph='glyphicon glyphicon-ok' /> : '' }<br/>
+                        <strong>Dates:</strong>&nbsp;{dates.start_date}&minus;{dates.end_date}
+                    </p>
+                    <p>
+                    <div dangerouslySetInnerHTML={{__html: description}}></div>
+                    <hr/>
+                    <ButtonToolbar>
+                        <Button bsStyle='primary'>Edit</Button> &nbsp;
+                        <Button bsStyle='danger'>Delete</Button>
+                    </ButtonToolbar>
+                    </p>
+                </Panel>
+        </Col>
         </Row>
     </Grid>
 )
